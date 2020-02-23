fbbfed5d77e473714378fa156f7ced2e6e13512d
Casper da Costa-Luis <casper.dcl@physics.org>
2020-02-23 17:40:33 +0000

    lint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Octokit } = require("@octokit/rest");
-const { fetch } = require("node-fetch");
+const fetch = require("node-fetch");
 
 const {
   INPUT_GIST_ID: gistID,
@@ -12,16 +12,16 @@ const {
 const icons = {
   rain: "🌧",
   snow: "❄",
-  sleet: "🌨",  // cloud snow
-  wind: "🌬",  // wind face
+  sleet: "🌨", // cloud snow
+  wind: "🌬", // wind face
   fog: "🌫",
   cloudy: "☁",
   clear_day: "🌣",
-  clear_night: "✨",  // sparkles
+  clear_night: "✨", // sparkles
   partly_cloudy_day: "🌥",
-  partly_cloudy_night: "●",  // black circle
+  partly_cloudy_night: "●", // black circle
   // maybe unused
-  hail: "⛈",  // cloud thunder rain
+  hail: "⛈", // cloud thunder rain
   thunderstorm: "🌩",
   tornado: "🌪",
   // unused
@@ -31,30 +31,27 @@ const icons = {
   cloud_thunder_rain: "⛈"
 };
 
-const API_BASE = 'https://api.darksky.net/forecast/';
+const API_BASE = "https://api.darksky.net/forecast/";
 
 async function main() {
   if (!gistID || !ghToken || !dSKey || !loc)
-    throw new Error('Must supply INPUT_(GIST_ID,GH_TOKEN,DARKSKY_KEY,LOCATION).');
-  let u;
-  if (!units) {
-    u = 'si';
-  } else {
-    u = units;
-  }
+    throw new Error(
+      "Must supply INPUT_(GIST_ID,GH_TOKEN,DARKSKY_KEY,LOCATION)."
+    );
+  const u = units ? units : "si";
   const API = `${API_BASE}${dSKey}/${loc}?units=${u}&exclude=hourly,daily`;
 
-  const json = await fetch(API).then((data) => data.json());
+  const json = await fetch(API).then(data => data.json());
   // console.debug(json);  // N.B.: contains private location
 
   // TODO: use flags, offset, alerts, currently.(
   //   summary icon precipIntensity precipIntensityError precipProbability
   //   precipType temperature apparentTemperature dewPoint humidity pressure
   //   windSpeed windGust windBearing cloudCover uvIndex visibility ozone)
-  const icon = icons[json.minutely.icon.replace('-', '_')];
+  const icon = icons[json.minutely.icon.replace("-", "_")];
   let intensities = [];
   let probabilities = [];
-  json.minutely.data.forEach((minute) => {
+  json.minutely.data.forEach(minute => {
     intensities.push(minute.precipIntensity);
     probabilities.push(minute.precipProbability);
   });
@@ -62,7 +59,7 @@ async function main() {
   let lines = [];
   lines.push(json.minutely.summary);
   lines.push("TODO: precipitation intensities & probabilities");
-  lines.push("https://darksky.net/poweredby/");  // Powered by Dark Sky
+  lines.push("https://darksky.net/poweredby/"); // Powered by Dark Sky
 
   console.log(lines.join("\n"));
 
