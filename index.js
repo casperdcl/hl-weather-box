08a43da592cf2ffe51e2407dd3ec55d6dc97419f
Casper da Costa-Luis <casper.dcl@physics.org>
2020-02-24 19:47:36 +0000

    combine 2 mins, add time

--- index.js
+++ index.js
@@ -34,6 +34,18 @@ const icons = {
 
 const API_BASE = "https://api.darksky.net/forecast/";
 
+function block_reduce_sum(array, elems = 2) {
+  return array
+    .map((val, i, ar) =>
+      ar.slice(Math.max(i - elems + 1, 0), i + 1).reduce((x, y) => x + y)
+    )
+    .filter((val, i) => (i + 1) % elems == 0);
+}
+
+function block_reduce_average(array, elems = 2) {
+  return block_reduce_sum(array, elems).map(i => i / elems);
+}
+
 async function main() {
   if (!gistID || !ghToken || !dSKey || !loc)
     throw new Error(
@@ -52,18 +64,29 @@ async function main() {
   let icon = icons[json.minutely.icon.replace("-", "_")];
   if (!icon) icon = icons.sun;
   let intensities = [];
-  let probStr = "";
+  let probabilities = [];
+  let endTime = 0;
   json.minutely.data.forEach(minute => {
+    probabilities.push(minute.precipProbability);
     intensities.push(minute.precipIntensity);
-    probStr +=
-      blocks[Math.round(minute.precipProbability * (blocks.length - 1))];
+    endTime = Math.max(endTime, minute.time);
   });
+  endTime = new Date(endTime * 1000);
+  probabilities = block_reduce_average(probabilities, 2);
+  intensities = block_reduce_average(intensities, 2);
+
   let intensMax = Math.max.apply(Math, intensities);
   if (!intensMax) intensMax = 1;
-  let intenStr = "";
+  let probStr = "Prb|";
+  probabilities.forEach(i => {
+    probStr += blocks[Math.round(i * (blocks.length - 1))];
+  });
+  probStr += `|${endTime.getHours() % 12}:${endTime.getMinutes()}`;
+  let intenStr = `${Math.round(intensMax * 10) / 10}|`.padStart(3);
   intensities.forEach(i => {
     intenStr += blocks[Math.round((i / intensMax) * (blocks.length - 1))];
   });
+  intenStr += "|";
 
   let lines = [];
   let { temperature, humidity, windSpeed } = json.currently;
